1d7741d8c1826d51cbb9770091140f9fdb2233b7
Eric Siebeneich <eric.siebeneich@gmail.com>
2019-07-16 19:56:12 -0500

    Upgrade bcrypt version

--- test/test-brypt-cache.js
+++ test/test-brypt-cache.js
@@ -11,7 +11,7 @@ var MockRedis = require('fakeredis');
 
 describe('Token Authentication Cache', function () {
   var token = 'token';
-  var tokenHash = '$2a$10$o4ezyhBv1b2tOQmHH1kiO.vdeIPLQQcY.1aCtH9VODTK.1/CpyRPa';
+  var tokenHash = '$2b$10$o4ezyhBv1b2tOQmHH1kiO.vdeIPLQQcY.1aCtH9VODTK.1/CpyRPa';
 
   function createMockRedis() {
     var r, p;
@@ -48,65 +48,65 @@ describe('Token Authentication Cache', function () {
 
     it('should store a key and retrieve it', function () {
       return t.set(tokenHash, token).
-      then(function (result) {
-        result.should.be.true;
-        return t.get(tokenHash);
-      }).
-      then(function (result) {
-        result.should.be.a('string');
-      });
+        then(function (result) {
+          result.should.be.true;
+          return t.get(tokenHash);
+        }).
+        then(function (result) {
+          result.should.be.a('string');
+        });
     });
 
     it('should not get a non-existant key', function () {
       return t.get('otherkey').
-      then(function (result) {
-        should.not.exist(result);
-      });
+        then(function (result) {
+          should.not.exist(result);
+        });
     });
 
     it('should verify a key from the cache', function () {
       return t.set(tokenHash, token).
-      then(function () {
-        return t.compare(tokenHash, token);
-      }).
-      then(function (result) {
-        // Make sure we didn't compare against `tokenHash`, but the cached hash
-        t._compare.should.have.been.called;
-        t._compare.lastCall.args[0].should.not.equal(tokenHash);
-        if (t instanceof TA.RedisBcryptCache) {
-          bcrypt.compare.lastCall.args[1].should.match(/^\$2a\$04\$/);
-        } else {
-          t._compare.lastCall.args[0].should.equal('94a08da1fecbb6e8b46990538c7b50b2');
-        }
-        result.should.be.true;
-      });
+        then(function () {
+          return t.compare(tokenHash, token);
+        }).
+        then(function (result) {
+          // Make sure we didn't compare against `tokenHash`, but the cached hash
+          t._compare.should.have.been.called;
+          t._compare.lastCall.args[0].should.not.equal(tokenHash);
+          if (t instanceof TA.RedisBcryptCache) {
+            bcrypt.compare.lastCall.args[1].should.match(/^\$2b\$04\$/);
+          } else {
+            t._compare.lastCall.args[0].should.equal('94a08da1fecbb6e8b46990538c7b50b2');
+          }
+          result.should.be.true;
+        });
     });
 
     it('should verify a key that is not in the cache', function () {
       return t.get(tokenHash).
-      then(function (result) {
-        should.not.exist(result);
-        return t.compare(tokenHash, token);
-      }).
-      then(function (result) {
-        result.should.be.true;
-        // Make sure that we're saving the result to the cache
-        t._compare.should.have.been.called;
-        t._compare.lastCall.args[0].should.equal(tokenHash);
-        t._compare.lastCall.args[1].should.equal(token);
-        if (t instanceof TA.RedisBcryptCache) {
-          bcrypt.compare.lastCall.args[1].should.equal(tokenHash);
-        }
-        return t.get(tokenHash);
-      }).
-      then(function (result) {
-        should.exist(result);
-        if (t instanceof TA.RedisBcryptCache) {
-          result.should.match(/^\$2a\$04\$/);
-        } else {
-          result.should.equal('94a08da1fecbb6e8b46990538c7b50b2');
-        }
-      });
+        then(function (result) {
+          should.not.exist(result);
+          return t.compare(tokenHash, token);
+        }).
+        then(function (result) {
+          result.should.be.true;
+          // Make sure that we're saving the result to the cache
+          t._compare.should.have.been.called;
+          t._compare.lastCall.args[0].should.equal(tokenHash);
+          t._compare.lastCall.args[1].should.equal(token);
+          if (t instanceof TA.RedisBcryptCache) {
+            bcrypt.compare.lastCall.args[1].should.equal(tokenHash);
+          }
+          return t.get(tokenHash);
+        }).
+        then(function (result) {
+          should.exist(result);
+          if (t instanceof TA.RedisBcryptCache) {
+            result.should.match(/^\$2b\$04\$/);
+          } else {
+            result.should.equal('94a08da1fecbb6e8b46990538c7b50b2');
+          }
+        });
     });
 
     it('should verify a key if lookup fails', function () {
@@ -127,44 +127,44 @@ describe('Token Authentication Cache', function () {
 
     it('should not verify a key that is not in the cache', function () {
       return t.get('key').
-      then(function (result) {
-        should.not.exist(result);
-        return t.compare('token123', tokenHash);
-      }).
-      then(function (result) {
-        result.should.be.false;
-      });
+        then(function (result) {
+          should.not.exist(result);
+          return t.compare('token123', tokenHash);
+        }).
+        then(function (result) {
+          result.should.be.false;
+        });
     });
 
     it('should not verify a key from the cache', function () {
       return t.set('key', token).
-      then(function () {
-        return t.compare('token123', tokenHash);
-      }).
-      then(function (result) {
-        result.should.be.false;
-      });
+        then(function () {
+          return t.compare('token123', tokenHash);
+        }).
+        then(function (result) {
+          result.should.be.false;
+        });
     });
 
     it('should remove a key', function () {
       return t.set('key', token).
-      then(function () {
-        return t.remove('key');
-      }).
-      then(function (result) {
-        result.should.be.true;
-        return t.get('key');
-      }).
-      then(function (result) {
-        should.not.exist(result);
-      });
+        then(function () {
+          return t.remove('key');
+        }).
+        then(function (result) {
+          result.should.be.true;
+          return t.get('key');
+        }).
+        then(function (result) {
+          should.not.exist(result);
+        });
     });
 
     it('should remove a non-existant key', function () {
       return t.remove('otherkey').
-      then(function (result) {
-        result.should.be.true;
-      });
+        then(function (result) {
+          result.should.be.true;
+        });
     });
   });
 
@@ -179,34 +179,34 @@ describe('Token Authentication Cache', function () {
 
       it('should prune expiring cache keys', function () {
         return t.set('key', token).
-        then(function () {
-          clock.travel(Date.now() + 2000);
-          return t.get('key');
-        }).
-        then(function (result) {
-          should.not.exist(result);
-        });
+          then(function () {
+            clock.travel(Date.now() + 2000);
+            return t.get('key');
+          }).
+          then(function (result) {
+            should.not.exist(result);
+          });
       });
 
       it('should reset expiration on get', function () {
         return t.set('key', token).
-        then(function () {
-          clock.travel(Date.now() + 900);
-          return t.get('key');
-        }).
-        then(function (result) {
-          should.exist(result);
-          clock.travel(Date.now() + 900);
-          return t.get('key');
-        }).
-        then(function (result) {
-          should.exist(result);
-          clock.travel(Date.now() + 2000);
-          return t.get('key');
-        }).
-        then(function (result) {
-          should.not.exist(result);
-        });
+          then(function () {
+            clock.travel(Date.now() + 900);
+            return t.get('key');
+          }).
+          then(function (result) {
+            should.exist(result);
+            clock.travel(Date.now() + 900);
+            return t.get('key');
+          }).
+          then(function (result) {
+            should.exist(result);
+            clock.travel(Date.now() + 2000);
+            return t.get('key');
+          }).
+          then(function (result) {
+            should.not.exist(result);
+          });
       });
     });
 
@@ -221,45 +221,45 @@ describe('Token Authentication Cache', function () {
       it('should prune expiring cache keys', function () {
         sinon.spy(t, '_prune');
         return t.set('key', token).
-        then(function () {
-          clock.tick(10000);
-          // Make sure the value was pruned before the get
-          t._prune.should.have.been.called;
-          _.has(t, "cache['3c6e0b8a9c15224a8228b9a98ca1531d']").should.be.false;
-          return t.get('key');
-        }).
-        then(function (result) {
-          should.not.exist(result);
-        });
+          then(function () {
+            clock.tick(10000);
+            // Make sure the value was pruned before the get
+            t._prune.should.have.been.called;
+            _.has(t, "cache['3c6e0b8a9c15224a8228b9a98ca1531d']").should.be.false;
+            return t.get('key');
+          }).
+          then(function (result) {
+            should.not.exist(result);
+          });
       });
 
       it('should reset expiration on get', function () {
         sinon.spy(t, '_get');
         sinon.spy(t, '_set');
         return t.set('key', token).
-        then(function () {
-          _.get(t, "_set.lastCall.thisValue.cache['3c6e0b8a9c15224a8228b9a98ca1531d']" +
-            ".expiration").should.equal(1000);
-          clock.tick(900);
-          return t.get('key');
-        }).
-        then(function (result) {
-          should.exist(result);
-          _.get(t, "_get.lastCall.thisValue.cache['3c6e0b8a9c15224a8228b9a98ca1531d']" +
-            ".expiration").should.equal(1900);
-          clock.tick(900);
-          return t.get('key');
-        }).
-        then(function (result) {
-          should.exist(result);
-          _.get(t, "_get.lastCall.thisValue.cache['3c6e0b8a9c15224a8228b9a98ca1531d']" +
-            ".expiration").should.equal(2800);
-          clock.tick(5000);
-          return t.get('key');
-        }).
-        then(function (result) {
-          should.not.exist(result);
-        });
+          then(function () {
+            _.get(t, "_set.lastCall.thisValue.cache['3c6e0b8a9c15224a8228b9a98ca1531d']" +
+              ".expiration").should.equal(1000);
+            clock.tick(900);
+            return t.get('key');
+          }).
+          then(function (result) {
+            should.exist(result);
+            _.get(t, "_get.lastCall.thisValue.cache['3c6e0b8a9c15224a8228b9a98ca1531d']" +
+              ".expiration").should.equal(1900);
+            clock.tick(900);
+            return t.get('key');
+          }).
+          then(function (result) {
+            should.exist(result);
+            _.get(t, "_get.lastCall.thisValue.cache['3c6e0b8a9c15224a8228b9a98ca1531d']" +
+              ".expiration").should.equal(2800);
+            clock.tick(5000);
+            return t.get('key');
+          }).
+          then(function (result) {
+            should.not.exist(result);
+          });
       });
     });
   });
